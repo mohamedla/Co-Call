cdea4d64ab17bfd0b8d0f246c34b7d582f945052
mAshraf <mAshraf@MASHRAF-LAP-EG>
2025-04-26 06:28:40 +0300

    text chat hub to sent messages

---
+++ CoCall/CoCall.API/Services/TextMessageCleanupService.cs
@@ -0,0 +1,34 @@
+﻿using CoCall.Data;
+using Microsoft.Extensions.Hosting;
+
+namespace CoCall.API.Services
+{
+    public class TextMessageCleanupService: BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public TextMessageCleanupService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<CoCallDbContext>();
+                    var cutoffDate = DateTime.UtcNow.AddDays(-7);
+
+                    var oldMessages = context.TextChatMessages.Where(m => m.Timestamp < cutoffDate);
+                    context.TextChatMessages.RemoveRange(oldMessages);
+
+                    await context.SaveChangesAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromHours(24), stoppingToken); // Run daily
+            }
+        }
+    }
+}
---
+++ CoCall/CoCall.Data/Models/TextChatMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoCall.Data.Models
+{
+    public class TextChatMessage
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
