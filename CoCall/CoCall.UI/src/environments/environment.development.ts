e99bc47fbefd0ba64d6ecd83450df026b8f36e97
mAshraf <mAshraf@MASHRAF-LAP-EG>
2025-04-26 07:13:52 +0300

    Frontend base structure

---
+++ CoCall/CoCall.UI/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  baseURL: 'localhost/api',
+  production: false,
+};
---
+++ CoCall/CoCall.UI/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  baseURL: 'localhost/api',
+  production: false,
+};
+
