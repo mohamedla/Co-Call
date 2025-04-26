42f6efcea6a639d42ba408e681c9d5bd0845be18
mAshraf <mAshraf@MASHRAF-LAP-EG>
2025-04-26 06:46:40 +0300

    video call expiry service to make call inactive depend on expiry date

---
+++ CoCall/CoCall.API/Services/CallExpiryService.cs
@@ -0,0 +1,40 @@
+﻿using CoCall.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace CoCall.API.Services
+{
+    public class CallExpiryService: BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CallExpiryService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<CoCallDbContext>();
+                    var expiredCalls = await context.VideoCalls
+                        .Where(c => c.IsActive && c.ExpireAt < DateTime.UtcNow)
+                        .ToListAsync();
+
+                    foreach (var call in expiredCalls)
+                    {
+                        call.IsActive = false;
+                        context.VideoCalls.Update(call);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken); // Check every 5 minutes
+            }
+        }
+    }
+}
